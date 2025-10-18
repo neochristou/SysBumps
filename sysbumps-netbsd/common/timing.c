67926198483983a6ad9361888370184687c36881
Neophytos Christou <neophytos_christou@brown.edu>
2025-10-18 16:17:37 -0400

    Port counting thread

--- sysbumps-netbsd/common/timing.c
+++ sysbumps-netbsd/common/timing.c
@@ -1,22 +1,18 @@
 #include "timing.h"
 
-void* counting(void* ctx){
-    __asm__ volatile(
-        "LDR x10, [%[ctx]]\n"       
-        "loop:\n"                      
-            "ADD x10, x10, #1\n"      
-            "STR x10, [%[ctx]]\n"   
-        "B loop\n"                      
-        : 
-        : [ctx] "r" (ctx) 
-        : "x10", "memory" 
-    );
+// XXX Maybe don't do it atomically
+void *counting(void *ctx) {
+  __asm__ volatile("loop:\n"
+                   "    LOCK INCQ (%[ctx])\n"
+                   "    JMP loop\n"
+                   :
+                   : [ctx] "r"(ctx)
+                   : "memory");
+  return NULL;
 }
 
-void start_timer(){
-    pthread_create(&counting_thread, NULL, counting, &timestamp);
+void start_timer() {
+  pthread_create(&counting_thread, NULL, counting, &timestamp);
 }
 
-void stop_timer(){
-    pthread_cancel(counting_thread);
-}
+void stop_timer() { pthread_cancel(counting_thread); }
